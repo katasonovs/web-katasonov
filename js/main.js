c389c9748b2d458b86259e43ab0f32ff78f9f857
katasonovs <79832870+katasonovs@users.noreply.github.com>
2022-05-28 10:06:37 +0500

    4
    4

---
+++ js/main.js
@@ -0,0 +1,120 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	var getHeight = function() {
+		var extraHeight = 0;
+
+		if ( isMobile.any() ) extraHeight = 50;
+		
+		setTimeout(function(){
+			$('#fh5co-main').stop().animate({
+				'height': $('.fh5co-tab-content.active').height() + extraHeight
+			});
+		}, 200);
+	};
+
+	var pieChart = function() {
+		$('.chart').easyPieChart({
+			scaleColor: false,
+			lineWidth: 10,
+			lineCap: 'butt',
+			barColor: '#17e7a4',
+			trackColor:	"#000000",
+			size: 160,
+			animate: 1000
+		});
+	};
+
+	var tabContainer = function() {
+		getHeight();
+		$(window).resize(function(){
+			getHeight();
+		})
+	};
+
+	var tabClickTrigger = function() {
+		$('.fh5co-tab-menu a').on('click', function(event) {
+			event.preventDefault();
+			var $this = $(this),
+				data = $this.data('tab'),
+				pie = $this.data('pie');
+
+			// add/remove active class
+			$('.fh5co-tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active');
+
+			$('.fh5co-tab-content.active').addClass('animated fadeOutDown');
+
+			setTimeout(function(){
+				$('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp');
+				$('.fh5co-tab-content[data-content="'+data+'"]').addClass('animated fadeInUp active');
+				getHeight();
+			}, 500);
+
+			if ( pie === 'yes' ) {
+				setTimeout(function(){
+					pieChart();
+				}, 800);
+			}
+			
+		})
+	};
+
+	// Document on load.
+	$(function(){
+		tabContainer();
+		tabClickTrigger();
+
+	});
+
+
+}());
+$(document).ready(function() {
+  console.log("Welcome to Duck Hunt!");
+
+  // Behaviour for the play again link
+  $('#play-again').click(function(e) {
+    $("#game-over").toggle();
+    new Game();
+  });
+
+ 
+  // Moves the crosshair with the mousepointer
+  // TODO: Add a mousemove event to the #game element...
+  $( "#game" ).mousemove(function( event ) {
+    $('#crosshair').css("left", event.pageX - 30)
+    $('#crosshair').css("top", event.pageY - 30)
+  });
+  // Kick-off a New Game
+  // TODO: Pass in a string to represent the difficulty level
+    function choose() {
+      var difficulty = $(this).text();
+      $('#game-start').toggle();
+      new Game(difficulty);
+    };
+    $('.difficulty').click(choose);
+});
